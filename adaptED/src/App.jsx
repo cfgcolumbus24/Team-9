514c6ff36f281713139fb0a33c726d6164f15584
Akshay Kollur <akshaykollur08@gmail.com>
2024-11-01 19:42:53 -0400

    Added AddClass, FieldPiece, and TeacherClassButton components

--- adaptED/src/App.jsx
+++ adaptED/src/App.jsx
@@ -1,53 +1,18 @@
-import React, { useState } from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TeacherClassButton from './Components/TeacherClassButton'
+import FieldPiece from './Components/FieldPiece'
 
 function App() {
-  const [formData, setFormData] = useState({
-    text1: '',
-    text2: ''
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form Data:', formData);
-  };
-
+  
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Text 1:
-            <input
-              type="text"
-              name="text1"
-              value={formData.text1}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Text 2:
-            <input
-              type="text"
-              name="text2"
-              value={formData.text2}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    <>
+      <FieldPiece></FieldPiece>
+      <TeacherClassButton></TeacherClassButton>
+    </>
+  )
 }
 
 export default App;

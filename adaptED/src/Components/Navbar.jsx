73dfcf66ccf7356e744b5928d43b2714c3834985
Brandon McGuckin <bmcguckin1@outlook.com>
2024-11-01 22:51:18 -0400

    Sticky Navbar

--- adaptED/src/Components/Navbar.jsx
+++ adaptED/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="bg-blue-500 p-4">
+        <nav className="bg-slate-500 p-4 sticky top-0 z-50">
             <div className="container mx-auto flex justify-between">
                 <h1 className="text-white text-lg font-bold">adaptED</h1>
                 <div className="flex space-x-4">

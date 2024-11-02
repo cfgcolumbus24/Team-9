51fb6b70b22184df4a0401c1ad1a4d7879e7a578
hyunbinjlee <hyunbinlee@u.northwestern.edu>
2024-11-02 01:10:22 -0400

    Fix imports

--- adaptED/src/App.jsx
+++ adaptED/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Retention from "./pages/Retention";
 import LessonPlanner from "./pages/LessonPlanner";
+import TeacherClassButton from "./components/TeacherClassButton";
+import PerClassRetentionTable from "./components/PerClassRetentionTable";
+import LessonPlanGenerator from "./components/LessonPlanGenerator";
 
 function App() {
   return (

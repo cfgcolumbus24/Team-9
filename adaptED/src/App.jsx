262b133dd03a4d9e39881383ef63aef396f8cb37
hyunbinjlee <hyunbinlee@u.northwestern.edu>
2024-11-01 22:20:05 -0400

    Add Google Authentication button

--- adaptED/src/App.jsx
+++ adaptED/src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import LessonPlanGenerator from "./components/LessonPlanGenerator";
+import "./App.css";
+import { signInWithGoogle } from "./components/config/firebase";
 
 const App = () => {
   return (
     <div>
       <h1>adaptED</h1>
       <LessonPlanGenerator />
+      <button
+        onClick={signInWithGoogle}
+        type="button"
+        class="login-with-google-btn"
+      >
+        Sign In With Google
+      </button>
     </div>
   );
 };

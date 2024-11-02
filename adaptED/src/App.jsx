aeb3bd352a89c2045cac8f99805adbeedbb4ab97
Rhea Jethvani <rhearjethvani@gmail.com>
2024-11-01 22:14:53 -0400

    Fixing home page layout and functionality

--- adaptED/src/App.jsx
+++ adaptED/src/App.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import LessonPlanGenerator from "./components/LessonPlanGenerator";
 
-import TeacherClassButton from './Components/TeacherClassButton'
-import FieldPiece from './Components/FieldPiece'
+import TeacherClassButton from './Components/TeacherClassButton';
 
 function App() {
   
   return (
     <div>
-      <FieldPiece></FieldPiece>
-      <TeacherClassButton></TeacherClassButton>
       <h1>adaptED</h1>
+
+      <div>
+      <h2>Your Classes</h2>
+      <TeacherClassButton 
+                className="Math 101" 
+                description="Introduction to Algebra" 
+            />
+      
+      <TeacherClassButton 
+                className="Math 101" 
+                description="Introduction to Algebra" 
+            />
+      
+      <TeacherClassButton 
+                className="Math 101" 
+                description="Introduction to Algebra" 
+            />
+      
+      <TeacherClassButton 
+                className="Math 101" 
+                description="Introduction to Algebra" 
+            />
+      </div>
+
       <LessonPlanGenerator />
     </div>
   );
